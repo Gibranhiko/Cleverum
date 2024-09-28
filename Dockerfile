15db8579976268ebf835cb4428f8c2a536429b31
Villarreal <gibran.villarreal@accenture.com>
2024-09-28 17:37:47 -0600

    update docker and app

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,37 @@
-# Use Node.js 18 with Alpine for a smaller image
-FROM node:21-alpine3.18 as builder
+# Etapa 1: Builder
+FROM node:18-alpine AS builder
 
 # Set working directory
 WORKDIR /app
 
+# Copiar package.json y package-lock.json
 COPY package*.json ./
 
-# Copy the rest of your app code (including package.json and package-lock.json)
-COPY . .
-
-# Install dependencies
+# Instalar las dependencias de desarrollo y producción
 RUN npm install
 
-# Build the application
+# Copiar el resto del código de la aplicación
+COPY . .
+
+# Construir la aplicación
 RUN npm run build
 
-# Set NODE_ENV to production for runtime
+# Etapa 2: Producción (Runtime)
+FROM node:18-alpine AS production
+
+# Setear la variable de entorno para producción
 ENV NODE_ENV=production
 
-# Expose the application port
+# Setear el directorio de trabajo en /app
+WORKDIR /app
+
+# Copiar solamente las dependencias necesarias para producción
+COPY --from=builder /app/node_modules ./node_modules
+COPY --from=builder /app/package*.json ./
+COPY --from=builder /app/dist ./dist
+
+# Exponer el puerto de la aplicación
 EXPOSE 3000
 
-# Command to run the application
+# Comando para ejecutar la aplicación
 CMD ["npm", "start"]
--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 const dev = process.env.NODE_ENV !== "production";
 const clientAdminApp = next({ dev, dir: "./src/client-admin" });
 const handle = clientAdminApp.getRequestHandler();
@@ -23,73 +23,77 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const main = async () => {
-  await clientAdminApp.prepare();
-  const app = express();
+  try {
+    await clientAdminApp.prepare();
+    const app = express();
 
-  // Create an HTTP server instance from Express
-  const httpWebServer = createServer(app);
+    // Create an HTTP server instance from Express
+    const httpWebServer = createServer(app);
 
-  // Connect to MongoDB
-  await connectToDatabase("orders");
-  console.log("Connected to MongoDB");
+    // Connect to MongoDB
+    await connectToDatabase("orders");
+    console.log("Connected to MongoDB");
 
-  // Serve QR code image
-  app.get("/getqr", (req, res) => {
-    const qrImagePath = path.join(__dirname, "../bot.qr.png");
+    // Serve QR code image
+    app.get("/getqr", (req, res) => {
+      const qrImagePath = path.join(__dirname, "../bot.qr.png");
 
-    fs.readFile(qrImagePath, (err, data) => {
-      if (err) {
-        console.log("Error reading the QR code image:", err);
-        return res.status(500).send("Error reading the QR code image");
-      }
-      res.contentType("image/png");
-      res.send(data);
-    });
-  });
-
-  // Handle Next.js app router requests
-  app.all("*", (req, res) => {
-    return handle(req, res);
-  });
-
-  // Bot routes or middleware
-  await createBot(
-    {
-      database: new MemoryDB(),
-      provider,
-      flow,
-    },
-    { extensions: { ai } }
-  );
-
-  // Initialize Socket.IO
-  const io = new Server(httpWebServer, {
-    cors: {
-      origin: "*", // Handle specific origin request on PROD
-      methods: ["GET", "POST"],
-    },
-  });
-
-  global.io = io;
-
-  // Handle WebSocket connections
-  io.on("connection", (socket) => {
-    console.log("New WebSocket connection:", socket.id);
-
-    socket.on("new-order", (order) => {
-      console.log("New order received:", order);
-      io.emit("new-order", order);
+      fs.readFile(qrImagePath, (err, data) => {
+        if (err) {
+          console.log("Error reading the QR code image:", err);
+          return res.status(500).send("Error reading the QR code image");
+        }
+        res.contentType("image/png");
+        res.send(data);
+      });
     });
 
-    socket.on("disconnect", () => {
-      console.log("Client disconnected:", socket.id);
+    // Handle Next.js app router requests
+    app.all("*", (req, res) => {
+      return handle(req, res);
     });
-  });
 
-  // Start the server
-  httpWebServer.listen(PORT, () => {
-    console.log(`Web server running on http://localhost:${PORT}`);
-  });
+    // Bot routes or middleware
+    await createBot(
+      {
+        database: new MemoryDB(),
+        provider,
+        flow,
+      },
+      { extensions: { ai } }
+    );
+
+    // Initialize Socket.IO
+    const io = new Server(httpWebServer, {
+      cors: {
+        origin: "*", // Handle specific origin request on PROD
+        methods: ["GET", "POST"],
+      },
+    });
+
+    global.io = io;
+
+    // Handle WebSocket connections
+    io.on("connection", (socket) => {
+      console.log("New WebSocket connection:", socket.id);
+
+      socket.on("new-order", (order) => {
+        console.log("New order received:", order);
+        io.emit("new-order", order);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Client disconnected:", socket.id);
+      });
+    });
+
+    // Start the server
+    httpWebServer.listen(PORT, () => {
+      console.log(`Web server running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.log("App could not start" + err);
+  }
 };
 
 main();
