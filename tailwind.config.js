e79df0eb2baebeed48664c3ceb3c0c99733885c6
Villarreal <gibran.villarreal@accenture.com>
2025-02-01 19:06:43 -0600

    update routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/client-admin/**/*.{js,ts,jsx,tsx}',
+    './src/client-admin/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
