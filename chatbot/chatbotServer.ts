ac782f9068e2fd3d219f91ea9f3486a41e430d40
Villarreal <gibran.villarreal@accenture.com>
2025-04-05 16:13:19 -0600

    remove socket for now

--- chatbot/chatbotServer.ts
+++ chatbot/chatbotServer.ts
@@ -1,41 +1,41 @@
 import "dotenv/config";
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 import { createBot, createProvider, MemoryDB } from "@builderbot/bot";
 import { BaileysProvider as Provider } from "@builderbot/provider-baileys";
 import AIClass from "./services/ai/index";
 import flow from "./flows";
 
 const BOT_PORT = process.env.BOT_PORT;
-const WEB_SOCKET_PORT = process.env.WEB_SOCKET_PORT;
-const WEB_SOCKET_URL = process.env.WEB_SOCKET_URL;
+// const WEB_SOCKET_PORT = process.env.WEB_SOCKET_PORT;
+// const WEB_SOCKET_URL = process.env.WEB_SOCKET_URL;
 const ai = new AIClass(process.env.OPEN_API_KEY, "gpt-4o");
 
 
-const socket = io(`${WEB_SOCKET_URL}:${WEB_SOCKET_PORT}/`, {
-  transports: ["websocket"],
-  reconnection: true,
-  reconnectionAttempts: 10,
-  reconnectionDelay: 5000,
-});
+// const socket = io(`${WEB_SOCKET_URL}:${WEB_SOCKET_PORT}/`, {
+//   transports: ["websocket"],
+//   reconnection: true,
+//   reconnectionAttempts: 10,
+//   reconnectionDelay: 5000,
+// });
 
-console.log(`Connecting to WebSocket server at: ${WEB_SOCKET_URL}:${WEB_SOCKET_PORT}`);
+// console.log(`Connecting to WebSocket server at: ${WEB_SOCKET_URL}:${WEB_SOCKET_PORT}`);
 
-socket.on("connect", () => { 
-  console.log("Connected to WebSocket server");
-});
+// socket.on("connect", () => { 
+//   console.log("Connected to WebSocket server");
+// });
 
-socket.on("disconnect", () => {
-  console.log("Disconnected from WebSocket server");
-});
+// socket.on("disconnect", () => {
+//   console.log("Disconnected from WebSocket server");
+// });
 
-// Log the reconnection attempts and errors
-socket.on("reconnect_attempt", (attempt) => {
-  console.log(`Reconnecting... Attempt ${attempt}`);
-});
+// // Log the reconnection attempts and errors
+// socket.on("reconnect_attempt", (attempt) => {
+//   console.log(`Reconnecting... Attempt ${attempt}`);
+// });
 
-socket.on("connect_error", (error) => {
-  console.error("WebSocket connection error:", error);
-});
+// socket.on("connect_error", (error) => {
+//   console.error("WebSocket connection error:", error);
+// });
 
 const main = async () => {
   try {
