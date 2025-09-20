ef147f42f275954768e252cc478f07a805eaa65b
Villarreal <gibran.villarreal@accenture.com>
2025-09-19 21:41:07 -0600

    qr per client id

--- web/app/api/chatbot/route.ts
+++ web/app/api/chatbot/route.ts
@@ -1,30 +1,40 @@
 export async function GET(req: Request) {
-   const BOT_URL = process.env.BOT_URL;
+   const url = new URL(req.url);
+   const server = url.searchParams.get('server');
 
-  try {
-    // Fetch the QR code image from the chatbot server
-    const response = await fetch(`${BOT_URL}`, {
-      cache: "no-store",
-    });
-    if (!response.ok) {
-      throw new Error("Failed to fetch QR code");
-    }
+   let BOT_URL;
+   if (server === '68cdc85e5eb788f7dcce041f') {
+     BOT_URL = 'http://localhost:4000';
+   } else if (server === '68cdc9a35eb788f7dcce042d') {
+     BOT_URL = 'http://localhost:4001';
+   } else {
+     BOT_URL = process.env.BOT_URL || 'http://localhost:4000';
+   }
 
-    // Convertimos la imagen a buffer
-    const imageBuffer = await response.arrayBuffer();
+   try {
+     // Fetch the QR code image from the chatbot server
+     const response = await fetch(`${BOT_URL}`, {
+       cache: "no-store",
+     });
+     if (!response.ok) {
+       throw new Error("Failed to fetch QR code");
+     }
 
-    return new Response(imageBuffer, {
-      status: 200,
-      headers: {
-        "Content-Type": "image/png",
-        "Cache-Control":
-          "no-store, no-cache, must-revalidate, proxy-revalidate",
-        Pragma: "no-cache",
-        Expires: "0",
-      },
-    });
-  } catch (error) {
-    console.error("Error fetching QR code:", error);
-    return new Response("Error fetching QR code", { status: 500 });
-  }
+     // Convertimos la imagen a buffer
+     const imageBuffer = await response.arrayBuffer();
+
+     return new Response(imageBuffer, {
+       status: 200,
+       headers: {
+         "Content-Type": "image/png",
+         "Cache-Control":
+           "no-store, no-cache, must-revalidate, proxy-revalidate",
+         Pragma: "no-cache",
+         Expires: "0",
+       },
+     });
+   } catch (error) {
+     console.error("Error fetching QR code:", error);
+     return new Response("Error fetching QR code", { status: 500 });
+   }
 }
