4692d6a7b3b5ac32f338aa30fbbb37112a95a48e
Villarreal <gibran.villarreal@accenture.com>
2025-09-23 10:35:59 -0600

    prevent submit

--- web/app/login/page.tsx
+++ web/app/login/page.tsx
@@ -30,7 +30,8 @@ export default function LoginPage() {
 
   const { loaders, setLoader, setState } = useAppContext();
 
-  const onSubmit = async (data: FormData) => {
+  const onSubmit = async (data: FormData, e?: any) => {
+    e?.preventDefault();
     setLoader("login", true);
 
     try {
@@ -62,7 +63,8 @@ export default function LoginPage() {
     } catch (error) {
       setErrorMessage("Error al iniciar sesión: " + error);
       setSuccessMessage(null);
-    } 
+      setLoader("login", false);
+    }
   };
 
   return (
