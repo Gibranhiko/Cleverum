c72a4283168ec7d2c55b0d3b06ecfe0b48375b63
Villarreal <gibran.villarreal@accenture.com>
2025-10-11 13:08:46 -0600

    add external to rollup

--- chatbot/rollup.config.js
+++ chatbot/rollup.config.js
@@ -1,13 +1,14 @@
 import typescript from 'rollup-plugin-typescript2'
 
 export default {
-    input: 'chatbotServer.ts',
-    output: {
-        file: 'dist/app.js',
-        format: 'esm',
-    },
-    onwarn: (warning) => {
-        if (warning.code === 'UNRESOLVED_IMPORT') return
-    },
-    plugins: [typescript()],
+  input: 'chatbotServer.ts',
+  output: {
+    file: 'dist/app.js',
+    format: 'esm',
+  },
+  external: ['googleapis', 'openai', 'mongoose', 'express'],
+  onwarn: (warning) => {
+    if (warning.code === 'UNRESOLVED_IMPORT') return
+  },
+  plugins: [typescript()],
 }
